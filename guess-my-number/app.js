51b73b907b3c2a3e892a1b5f27eb59ff1ace1888
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-07-16 00:00:12 +0430

    game is ready

--- guess-my-number/app.js
+++ guess-my-number/app.js
@@ -1,12 +1,70 @@
-var randomNumber = Math.floor(Math.random() + 1 * 20);
 var guessInput = document.querySelector(".input-guess");
 var guessButton = document.querySelector(".button-guess");
 var form = document.querySelector("form");
+var statusParagraph = document.querySelector(".status-indicator");
+var section = document.querySelector("section");
+var currentBox = document.querySelector(".currect-box");
+var chanceLeft = document.querySelector(".chance-counter");
+var highestScore = document.querySelector(".highest-score");
+var resetBtn = document.querySelector(".reset");
+var randomNumber = Math.floor(Math.random() * 20) + 1;
+console.log(randomNumber);
+var chances = 5;
+var takes = 0;
+chanceLeft.textContent = "".concat(chances);
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 });
-// typescript for event on click
-guessButton === null || guessButton === void 0 ? void 0 : guessButton.addEventListener("click", function (e) {
-    var guess = parseInt(guessInput.value);
-    console.log(guess);
+resetBtn.addEventListener("click", function () {
+    resetGame();
 });
+guessButton.addEventListener("click", function () {
+    if (chances > 0) {
+        var guess = parseInt(guessInput.value);
+        game(guess);
+        chances--;
+        chanceLeft.textContent = "".concat(chances);
+    }
+    if (chances === 0) {
+        resetBtn.focus();
+        statusParagraph.classList.add("text-red-500");
+        statusParagraph.textContent = "You lost!";
+        currentBox.classList.remove("bg-red-200", "bg-yellow-200", "bg-green-500");
+        currentBox.classList.add("bg-red-500");
+        currentBox.classList.add("text-red-100");
+        guessButton.disabled = true;
+    }
+});
+function game(guess) {
+    currentBox.textContent = guess.toString();
+    takes++;
+    if (guess === randomNumber) {
+        statusParagraph.textContent = "You guessed correctly!";
+        currentBox.classList.remove("bg-red-200", "bg-yellow-200");
+        currentBox.classList.add("bg-green-500");
+        resetBtn.focus();
+        guessButton.disabled = true;
+        if (takes < chances) {
+            highestScore.textContent = "".concat(takes);
+        }
+    }
+    else if (guess > randomNumber) {
+        statusParagraph.textContent = "You guessed  high!";
+        currentBox.classList.add("bg-red-200");
+    }
+    else {
+        statusParagraph.textContent = "You guessed  low!";
+        currentBox.classList.add("bg-yellow-200");
+    }
+}
+function resetGame() {
+    randomNumber = Math.floor(Math.random() * 20) + 1;
+    chances = 5;
+    takes = 0;
+    chanceLeft.textContent = "".concat(chances);
+    statusParagraph.textContent = "";
+    currentBox.classList.remove("bg-red-500", "bg-green-500", "bg-yellow-200");
+    currentBox.classList.add("bg-white");
+    currentBox.classList.add("text-green-800");
+    guessButton.disabled = false;
+}
--- guess-my-number/app.ts
+++ guess-my-number/app.ts
@@ -1,13 +1,81 @@
-const randomNumber = Math.floor(Math.random() + 1 * 20);
-const guessInput = document.querySelector(".input-guess");
-const guessButton = document.querySelector(".button-guess");
-const form = document.querySelector("form");
+const guessInput = document.querySelector(".input-guess") as HTMLInputElement;
+const guessButton = document.querySelector(
+  ".button-guess"
+) as HTMLButtonElement;
+const form = document.querySelector("form") as HTMLFormElement;
+const statusParagraph = document.querySelector(
+  ".status-indicator"
+) as HTMLLIElement;
+const section = document.querySelector("section") as HTMLElement;
+const currentBox = document.querySelector(".currect-box") as HTMLElement;
+const chanceLeft = document.querySelector(".chance-counter") as HTMLElement;
+const highestScore = document.querySelector(".highest-score") as HTMLElement;
+const resetBtn = document.querySelector(".reset") as HTMLElement;
 
-form!.addEventListener("submit", e => {
+let randomNumber = Math.floor(Math.random() * 20) + 1;
+console.log(randomNumber);
+let chances = 5;
+let takes = 0;
+
+chanceLeft.textContent = `${chances}`;
+form.addEventListener("submit", e => {
   e.preventDefault();
 });
-// typescript for event on click
-guessButton?.addEventListener("click", () => {
-  const guess = parseInt(guessInput!.value);
-  console.log(guess);
+
+resetBtn.addEventListener("click", () => {
+  resetGame();
 });
+
+guessButton.addEventListener("click", () => {
+  if (chances > 0) {
+    const guess = parseInt(guessInput.value);
+    game(guess);
+    chances--;
+    chanceLeft.textContent = `${chances}`;
+  }
+
+  if (chances === 0) {
+    resetBtn.focus();
+    statusParagraph.classList.add("text-red-500");
+
+    statusParagraph.textContent = "You lost!";
+    currentBox.classList.remove("bg-red-200", "bg-yellow-200", "bg-green-500");
+    currentBox.classList.add("bg-red-500");
+    currentBox.classList.add("text-red-100");
+    guessButton.disabled = true;
+  }
+});
+
+function game(guess: number) {
+  currentBox.textContent = guess.toString();
+  takes++;
+  if (guess === randomNumber) {
+    statusParagraph.textContent = "You guessed correctly!";
+    currentBox.classList.remove("bg-red-200", "bg-yellow-200");
+    currentBox.classList.add("bg-green-500");
+    resetBtn.focus();
+    guessButton.disabled = true;
+
+    if (takes < chances) {
+      highestScore.textContent = `${takes}`;
+    }
+  } else if (guess > randomNumber) {
+    statusParagraph.textContent = "You guessed  high!";
+    currentBox.classList.add("bg-red-200");
+  } else {
+    statusParagraph.textContent = "You guessed  low!";
+    currentBox.classList.add("bg-yellow-200");
+  }
+}
+
+function resetGame() {
+  randomNumber = Math.floor(Math.random() * 20) + 1;
+  chances = 5;
+  takes = 0;
+  chanceLeft.textContent = `${chances}`;
+  statusParagraph.textContent = "";
+  currentBox.classList.remove("bg-red-500", "bg-green-500", "bg-yellow-200");
+  currentBox.classList.add("bg-white");
+  currentBox.classList.add("text-green-800");
+  guessButton.disabled = false;
+}
