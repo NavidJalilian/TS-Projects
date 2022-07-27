7f54bd812bd9448b835ade1e50e27a1c2ebf360f
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-07-27 19:49:40 +0430

    stating ui of form is done

---
+++ formValidation/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // include index.html and ./output/app.js in the bundle
+  content: ["./**/*.html", "./output/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
