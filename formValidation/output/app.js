eefc3cc8590478fcac432b17507932afc3d4221d
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-09-18 15:28:16 +0430

    adding eye input indicator

--- formValidation/output/app.js
+++ formValidation/output/app.js
@@ -2,18 +2,22 @@
 const statusDiv = document.querySelector(".status");
 const button = document.querySelector("button");
 const form = document.querySelector("form");
-// button.addEventListener("click", () => {
-//   statusDiv.classList.toggle("scale-100");
-// });
-
+const eyePassword = document.querySelector(".eye-container");
+const passwordInput = form[4];
 button.addEventListener("click", () => {
   for (const input of form) {
     if (input.validity.valid) continue;
     statusDiv.textContent = "Erorr: " + input.name + " is not Valid!";
     statusDiv.classList.add("scale-100");
-    if (!input.validity.valid) return;
+    return;
   }
 });
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
+eyePassword.onclick = () => {
+  if (passwordInput.type === "password") passwordInput.type = "text";
+  else passwordInput.type = "password";
+};
+//password => text
+// text=>pass
